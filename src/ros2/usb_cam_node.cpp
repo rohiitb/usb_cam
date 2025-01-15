11b0c2e9da879dc61bec735fe41faab156be7b7e
Rohit Bhikule <bhikulerohit@gmail.com>
2025-01-15 14:39:12 -0500

    take_and sen image directly

--- src/ros2/usb_cam_node.cpp
+++ src/ros2/usb_cam_node.cpp
@@ -438,9 +438,9 @@ void UsbCamNode::update()
     // bool isSuccessful = (m_parameters.pixel_format_name == "raw_mjpeg") ?
     //   take_and_send_image_mjpeg() :
     //   take_and_send_image();
-    if (!isSuccessful) {
-      RCLCPP_WARN_ONCE(this->get_logger(), "USB camera did not respond in time.");
-    }
+    // if (!isSuccessful) {
+    //   RCLCPP_WARN_ONCE(this->get_logger(), "USB camera did not respond in time.");
+    // }
   }
 }
 }  // namespace usb_cam
